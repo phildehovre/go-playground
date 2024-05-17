1f8ff97a870f28e24dc2cab26b207eda2130ea5b
philippedehovre <ph.dehovre@gmail.com>
2024-05-17 08:35:36 +0100

    Add basic server

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parseform() err: %v, err")
+		return
+	}
+	fmt.Fprintf(w, "Post request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "hello")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+}
--- structs.go
+++ structs.go
@@ -1,38 +1,38 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"reflect"
-)
+// import (
+// 	"fmt"
+// 	"reflect"
+// )
 
-type Vertex struct {
-	X, Y int
-}
+// type Vertex struct {
+// 	X, Y int
+// }
 
-var array = [5]string{"hello", "world", "How", "are", "you"}
+// var array = [5]string{"hello", "world", "How", "are", "you"}
 
-var zeroed = make([]int, 0, 2)
-var zeroedFull = make([]int, 2, 2)
+// var zeroed = make([]int, 0, 2)
+// var zeroedFull = make([]int, 2, 2)
 
-var (
-	v1 = Vertex{1, 2}  // has type Vertex
-	v2 = Vertex{X: 1}  // Y:0 is implicit
-	v3 = Vertex{}      // X:0 and Y:0
-	p  = &Vertex{1, 2} // has type *Vertex
-)
+// var (
+// 	v1 = Vertex{1, 2}  // has type Vertex
+// 	v2 = Vertex{X: 1}  // Y:0 is implicit
+// 	v3 = Vertex{}      // X:0 and Y:0
+// 	p  = &Vertex{1, 2} // has type *Vertex
+// )
 
-func main() {
+// func main() {
 
-	pointer := &array
-	slice := pointer[0:3]
-	slice[0] = "hi"
-	// for _, value := range array {
-	// 	fmt.Println(value)
-	// }
-	printArraySlice(zeroed)
-	printArraySlice(zeroedFull)
-}
+// 	pointer := &array
+// 	slice := pointer[0:3]
+// 	slice[0] = "hi"
+// 	// for _, value := range array {
+// 	// 	fmt.Println(value)
+// 	// }
+// 	printArraySlice(zeroed)
+// 	printArraySlice(zeroedFull)
+// }
 
-func printArraySlice(array []int) {
-	fmt.Printf("Array: cap: %v, len: %v, type: %v,  %v\n", cap(array), len(array), reflect.TypeOf(array), array)
-}
+// func printArraySlice(array []int) {
+// 	fmt.Printf("Array: cap: %v, len: %v, type: %v,  %v\n", cap(array), len(array), reflect.TypeOf(array), array)
+// }
